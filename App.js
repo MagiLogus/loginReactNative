b63008f6d22704d907963d84ce19b89a50dbda8b
Paulo Oliveira <paulo.oliveira.phgo@gmail.com>
2024-01-28 00:14:16 -0300

    .

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import { StyleSheet, Text, ImageBackground, View, StatusBar, SafeAreaView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, Image, StatusBar, SafeAreaView, View, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-
 
 export default function App() {
   return (
@@ -11,30 +10,72 @@ export default function App() {
         style={styles.background}
       >
         <StatusBar translucent backgroundColor="transparent" />
-        <ImageBackground
-          source={require('./src/assets/images/background.png')}
-          style={styles.backgroundImage}
-          resizeMode="contain"
+        <KeyboardAvoidingView
+          style={{ flex: 1, width: '100%' }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
         >
-
-
-
-
-
-
-
-
-        </ImageBackground>
-
-
-
-
-
-
-
-
-
-
+          <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={styles.backgroundImage}>
+              <Image
+                style={styles.logo}
+                source={require('./src/assets/images/logo.png')}
+              />
+              <Text style={styles.welcomeText}>Bem Vindo !</Text>
+              <Text style={styles.infoText}>Ao fazer login você concorda com nossos <Text style={styles.infoTextNotice}>Termos e Política de Privacidade</Text></Text>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Email"
+                />
+                <Image
+                  source={require('./src/assets/images/mail.png')}
+                  style={styles.inputIcon}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Senha"
+                  secureTextEntry={true}
+                />
+                <Image
+                  source={require('./src/assets/images/lock.png')}
+                  style={styles.inputIcon}
+                />
+              </View >
+              <Text style={styles.pwdText}>Esqueceu a Senha ?</Text>
+              <View style={styles.btnContainer}>
+                <TouchableOpacity style={styles.btnLogin}>
+                  <Text style={styles.btnLoginText}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btnRegister}>
+                  <Text style={styles.btnRegisterText}>Registrar</Text>
+                </TouchableOpacity>
+              </View >
+              <Text style={styles.textConect}>Conectar com</Text>
+              <Image
+                source={require('./src/assets/images/id.png')}
+                style={styles.idIcon}
+              />
+              <Text style={styles.textConectSocial}>ou conectar com</Text>
+              <View style={styles.socialLoginContainer}>
+                <Image
+                  source={require('./src/assets/images/facebook.png')}
+                  style={styles.socialIcon}
+                />
+                <Image
+                  source={require('./src/assets/images/google.png')}
+                  style={styles.socialIcon}
+                />
+                <Image
+                  source={require('./src/assets/images/linkedin.png')}
+                  style={styles.socialIcon}
+                />
+              </View>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
       </LinearGradient>
     </SafeAreaView>
   );
@@ -46,27 +87,145 @@ const styles = StyleSheet.create({
   },
   background: {
     width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 35,
-    paddingBottom: 10,
+    paddingTop: 25,
   },
   backgroundImage: {
     alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: "100%",
+    width: '90%',
+    height: "95%",
     margin: 10,
+    backgroundColor: '#fff',
+    borderRadius: 20,
   },
   logo: {
-
+    marginTop: 29,
+    width: 130,
+    height: 104,
   },
-
-  overlayContent: {
-
+  welcomeText: {
+    width: '90%',
+    textAlign: 'center',
+    marginTop: 34,
+    color: '#000',
+    fontSize: 21,
+    fontStyle: 'normal',
+    fontWeight: '400',
   },
-  text: {
-
+  infoText: {
+    width: '90%',
+    marginTop: 16,
+    color: '#6B5E5E',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    textAlign: 'center',
   },
-
+  infoTextNotice: {
+    width: '90%',
+    marginTop: 16,
+    color: '#043E38',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#CCC9C9',
+    backgroundColor: '#F9F9F9',
+    borderRadius: 9,
+    width: '90%',
+    marginTop: 23,
+    height: 47,
+  },
+  inputIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    paddingLeft: 17
+  },
+  pwdText: {
+    textAlign: 'right',
+    marginTop: 20,
+    marginRight: 33,
+    width: '90%',
+    color: '#043E38'
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    marginTop: 22,
+    justifyContent: 'space-around',
+    width: '90%',
+    alignSelf: 'center',
+  },
+  btnLogin: {
+    backgroundColor: '#043E38',
+    width: 151,
+    height: 45,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#043E38'
+  },
+  btnLoginText: {
+    color: '#fff',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400'
+  },
+  btnRegister: {
+    backgroundColor: '#FFF',
+    width: 151,
+    height: 45,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#043E38'
+  },
+  btnRegisterText: {
+    color: '#043E38',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400'
+  },
+  textConect: {
+    marginTop: 21,
+    color: '#6B5E5E',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400'
+  },
+  idIcon: {
+    marginTop: 17,
+    width: 69,
+    height: 72,
+  },
+  textConectSocial: {
+    marginTop: 11,
+    color: '#6B5E5E',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400'
+  },
+  socialLoginContainer: {
+    flexDirection: 'row',
+    marginTop: 12,
+    gap: 18,
+    width: '90%',
+    justifyContent: 'center',
+  },
+  socialIcon: {
+    width: 39,
+    height: 39,
+  },
 });
